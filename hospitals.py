76865625cebbeed99f7b5113ecab40ba3723d753
mud00 <96502981+mud00@users.noreply.github.com>
2023-06-09 01:11:19 +0200

    Update hospitals.py

--- hospitals.py
+++ hospitals.py
@@ -17,7 +17,7 @@ import plotly.express as px
 import re
 
 st.title('French Hospital Analysis')
-st.write('Our analysis is based on hospitals in France and contains information about their locations and specialties, each hospital is represented as a point on the map. The dataset includes hospitals with different specialties, and some hospitals may have multiple specialties separated by semicolons. \n\nThe interactive map allows filtering the hospitals based on the selected specialty or showcasing all hospitals.\n\n **Additionally, hospitals without a specified specialty are grouped under the "No Specialty" option.**')
+st.write('*Our analysis is based on hospitals in France and contains information about their locations and specialties, each hospital is represented as a point on the map. The dataset includes hospitals with different specialties, and some hospitals may have multiple specialties separated by semicolons. \n\nThe interactive map allows filtering the hospitals based on the selected specialty or showcasing all hospitals.\n\n **Additionally, hospitals without a specified specialty are grouped under the "No Specialty" option.** *')
 st.subheader('Summary Statistics')
 
 
