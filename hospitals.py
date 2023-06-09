03c2afb9106e3e73cec45f5d59b90933a6395daf
mud00 <96502981+mud00@users.noreply.github.com>
2023-06-09 10:42:28 +0200

    Update hospitals.py

--- hospitals.py
+++ hospitals.py
@@ -16,8 +16,8 @@ from streamlit_folium import folium_static
 import plotly.express as px
 import re
 
-st.title('French Hospital Analysis')
-st.write('Our analysis is based on hospitals in France 🇫🇷 and contains information about their locations and specialties, each hospital is represented as a point on the map. \n\nThe dataset includes hospitals with different specialties, and some hospitals may have multiple specialties separated by semicolons. \n\nThe interactive map allows filtering the hospitals based on the selected specialty or showcasing all hospitals.\n\n **Additionally, hospitals without a specified specialty are grouped under the "No Specialty" option.**')
+st.title('French Hospital Analysis 🇫🇷 ')
+st.write('Our analysis is based on hospitals in France and contains information about their locations and specialties, each hospital is represented as a point on the map. \n\nThe dataset includes hospitals with different specialties, and some hospitals may have multiple specialties separated by semicolons. \n\nThe interactive map allows filtering the hospitals based on the selected specialty or showcasing all hospitals.\n\n **Additionally, hospitals without a specified specialty are grouped under the "No Specialty" option.**')
 st.subheader('Summary Statistics')
 
 
