2079195bad628d571cab4ea32383aeccd656c867
mud00 <96502981+mud00@users.noreply.github.com>
2023-06-09 16:20:57 +0200

    Update hospitals.py

--- hospitals.py
+++ hospitals.py
@@ -227,7 +227,7 @@ chart = chart.configure_axis(
 )
 
 st.altair_chart(chart)
-st.caption("We can see that intensive care is the most important, most dominant specialty in french hospitals, occupying the top position with a representation of **36% among all hospitals**.")
+st.caption("We can see that **intensive care** is the most important, most dominant specialty in french hospitals, occupying the top position with a representation of **36% among all hospitals**.")
 st.write('\n\n')
 word_weights = {word: count for word, count in zip(labels, counts)}
 
